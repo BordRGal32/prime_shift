35ded05a7b9ace564ff51312f577b39aeb12ae02
BordRGal32 <lauren.y@theclymb.com>
2014-07-14 10:05:38 -0700

    Update prime_shifting.rb

--- lib/prime_shifting.rb
+++ lib/prime_shifting.rb
@@ -1,18 +1,3 @@
-# def prime_shifting(number)
-
-# number_arrays = (1..number).to_a
-# prime = 2
-
-# prime.upto(number_arrays.length) do |prime|
-#   number_arrays.each do |element|
-#     if (element % prime == 0 && prime != element)
-#       number_arrays.delete(element)
-#     end 
-#   end
-# end 
-  
-#   number_arrays
-# end
 
 def prime_shifting(number)
 number_arrays = (2..number).to_a 
